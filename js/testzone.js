8dbe6e13518fc8ab482678744766e848ca5a1f87
jonathan-crepeau <jon.crepeau@gmail.com>
2022-04-07 17:56:55 -0700

    Added methods to game object, updated UI performance

--- js/testzone.js
+++ js/testzone.js
@@ -40,10 +40,14 @@ class Battleship extends Ship {
   }
   attack(){
     if (Math.random() < this.accuracy) {
-      console.log('Battleship attack!');
-      game.alienShips[0].hull -= this.firepower;
+      console.log('[Battleship]: "Direct hit on Alien Vessel, Captain!"');
+      game.alienShips[0].hull -= this.firepower
+      console.log(
+        `--------------------------\nBattleship Hull: ${user.hull}\nAlien Hull: ${game.alienShips[0].hull}\n--------------------------`
+      );
+      
     } else {
-      console.log('Your laser missed!');
+      console.log('[Battleship]: "Lasers missed the target, Captain!"');
     }
   }
 }
@@ -53,20 +57,55 @@ class Alien extends Ship {
     super(name);
     this.hull = Math.floor(Math.random() * (6 - 3) + 3);
     this.firepower = Math.floor(Math.random() * (4 - 2) + 2);
-    this.accuracy = (Math.random() * (0.8 - 0.7) + 0.7).toFixed(2);
+    this.accuracy = (Math.random() * (0.8 - 0.6) + 0.6).toFixed(2);
   }
   attack(){
     if (Math.random() < this.accuracy) {
-    console.log('Alien attack!');
+    console.log('[Alien]: "Direct hit on Battleship vessel, overlord!"');
     user.hull -= this.firepower;
+    console.log(
+      `--------------------------\nBattleship Hull: ${user.hull}\nAlien Hull: ${game.alienShips[0].hull}\n--------------------------`
+    );
     } else {
-      console.log('Alien laser missed!');
+      console.log('[Alien]: "Acch! -- We missed!"');
     }
   }
 }
 
 const game = {
   alienShips: [],
+  playGame(humanPlayer) {
+    if (humanPlayer.hull > 0 && game.alienShips.length > 0) {
+      game.battle(humanPlayer, game.alienShips[0]);
+    } else {
+      console.log('Game over --');
+    }
+  },
+  battle(player1, player2) {
+    console.log("[======= START OF BATTLE =======]");
+    console.log(`[STARTING BATTLESHIP HULL]: ${user.hull}\n[STARTING ALIEN HULL]: ${game.alienShips[0].hull}`)
+    while (player1.hull > 0 && player2.hull > 0) {
+      player1.attack();
+      player2.attack();
+    }
+    console.log("[======= END OF BATTLE =======]");
+    console.log(user);
+    game.alienShips.splice(0, 1);
+    if (game.alienShips.length > 0 && user.hull > 0) {
+      let response = prompt('Do you want to attack the next ship?').toLowerCase();
+      console.log(response);
+      if (response == 'yes') {
+        console.log("[======= Next ship! =======]");
+        return game.battle(user, game.alienShips[0]);
+      } else {
+        console.log('Retreat!');
+      }
+    } else if (game.alienShips.length == 0) {
+      return console.log('Winner! All alien ships exterminated!');
+    } else {
+      return console.log('[======= Game Over! =======]');
+    }
+  },
 };
 
 
@@ -83,36 +122,36 @@ for (let a = 0; a < 6; a++) {
 
 
 // SECTION - Functions:
-function playGame(humanPlayer) {
-  if (humanPlayer.hull > 0 && game.alienShips.length > 0) {
-    battle(humanPlayer, game.alienShips[0]);
-  } else {
-    console.log('Game over --');
-  }
-}
+// function playGame(humanPlayer) {
+//   if (humanPlayer.hull > 0 && game.alienShips.length > 0) {
+//     battle(humanPlayer, game.alienShips[0]);
+//   } else {
+//     console.log('Game over --');
+//   }
+// }
 
-function battle(player1, player2) {
-  while (player2.hull > 0) {
-    player1.attack();
-    player2.attack();
-  }
-  console.log(user);
-  game.alienShips.splice(0, 1);
-  if (game.alienShips.length > 0 && user.hull > 0) {
-    let response = prompt('Do you want to attack the next ship?').toLowerCase();
-    console.log(response);
-    if (response == 'yes') {
-      console.log('Next ship!');
-      return battle(user, game.alienShips[0]);
-    } else {
-      console.log('Retreat!');
-    }
-  } else if (game.alienShips.length == 0) {
-    console.log('Winner! All alien ships exterminated!');
-  } else {
-    console.log('Game over!')
-  }
-}
+// function battle(player1, player2) {
+//   while (player2.hull > 0) {
+//     player1.attack();
+//     player2.attack();
+//   }
+//   console.log(user);
+//   game.alienShips.splice(0, 1);
+//   if (game.alienShips.length > 0 && user.hull > 0) {
+//     let response = prompt('Do you want to attack the next ship?').toLowerCase();
+//     console.log(response);
+//     if (response == 'yes') {
+//       console.log('Next ship!');
+//       return battle(user, game.alienShips[0]);
+//     } else {
+//       console.log('Retreat!');
+//     }
+//   } else if (game.alienShips.length == 0) {
+//     console.log('Winner! All alien ships exterminated!');
+//   } else {
+//     console.log('Game over!')
+//   }
+// }
 
 // NOTE - Start game:
-// playGame(user);
+game.playGame(user);
